ad0dce74ad0160ab5fc3b50b83001be4adc7a940
Daniil <77277774+shamemask@users.noreply.github.com>
2024-07-05 20:43:38 +0300

    перевел в ts

---
+++ src/components/index.d.ts
@@ -0,0 +1,15 @@
+declare module './components' {
+    export { Button } from './button';
+    export { ChatItem } from './chat-item';
+    export { ChatList } from './chat-list';
+    export { Dialog } from './dialog';
+    export { Input } from './input';
+    export { InputField } from './input-field';
+    export { Link } from './link';
+    export { PageTitle } from './page-title';
+    export { InputWithLabel } from './input-with-label';
+    export { Interface } from './interface';
+    export { Hr } from './hr';
+    export { Main } from './main';
+    export { Image } from './image';
+}
