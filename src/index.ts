87b4dcc5597b4f3775b72658ab683ad157d4f39e
Daniil <77277774+shamemask@users.noreply.github.com>
2024-07-05 20:52:38 +0300

    Доработал аннотацию

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,32 @@ import Handlebars from 'handlebars';
 import * as Components from './components';
 import * as Pages from './pages';
 
-const pages = {
-    'chat': [ Pages.ChatPage ],
-    'login': [ Pages.LoginPage ],
-    'register': [ Pages.RegisterPage ],
-    'profile': [ Pages.ProfilePage ],
-    '404': [ Pages.NotFoundPage ],
-    '5xx': [ Pages.ErrorPage ]
+type PageType = 'chat' | 'login' | 'register' | 'profile' | '404' | '5xx';
+
+const pages: Record<PageType, [string, any?]> = {
+    'chat': [Pages.ChatPage],
+    'login': [Pages.LoginPage],
+    'register': [Pages.RegisterPage],
+    'profile': [Pages.ProfilePage],
+    '404': [Pages.NotFoundPage],
+    '5xx': [Pages.ErrorPage]
 };
 
-Object.entries(Components).forEach(([ name, component ]) => {
+Object.entries(Components).forEach(([name, component]) => {
     Handlebars.registerPartial(name, component);
 });
 
-function navigate(page) {
-    const [ source, args ] = pages[page];
+function navigate(page: PageType) {
+    const [source, args] = pages[page];
     const handlebarsFunct = Handlebars.compile(source);
     document.body.innerHTML = handlebarsFunct(args);
 }
 
 document.addEventListener('DOMContentLoaded', () => navigate('login'));
 
-document.addEventListener('click', e => {
-    const page = e.target.getAttribute('page');
+document.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    const page = target.getAttribute('page') as PageType | null;
     if (page) {
         navigate(page);
 
---
+++ src/pages/pages.d.ts
@@ -0,0 +1,8 @@
+declare module './pages' {
+    export const ChatPage: string;
+    export const LoginPage: string;
+    export const RegisterPage: string;
+    export const ProfilePage: string;
+    export const NotFoundPage: string;
+    export const ErrorPage: string;
+}
